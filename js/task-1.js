4bb75262ed5cddc94a365cc773cc7339fff1c808
ZatsepaTanya <Tanyamath@i.ua>
2021-01-26 21:34:04 +0200

    task-6

--- js/task-1.js
+++ js/task-1.js
@@ -23,9 +23,16 @@ import users from "./users.js";
     console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
     
     // task-5
-    const getUserWithEmail = (users, email) => {
-      // твой код
-    };
+    const getUserWithEmail = (users, email) => users.find(user => user.email === email);
     
     console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
     console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+   
+    // task-6
+    const getUsersWithAge = (users, min, max) => users.filter(user => user.age>=min && user.age<=max); 
+    console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+    
+    console.log(getUsersWithAge(users, 30, 40));
+    // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+    // task-7
